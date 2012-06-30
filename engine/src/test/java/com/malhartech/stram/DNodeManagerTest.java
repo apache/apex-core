fda340547450c9a8909682eeaca7eb00c81d09e6
hdev <thomas@malhar-inc.com>
2012-06-29 19:21:33 -0700

    Modified test case to verify buffer server address assignment.

--- engine/src/test/java/com/malhartech/stram/DNodeManagerTest.java
+++ engine/src/test/java/com/malhartech/stram/DNodeManagerTest.java
@@ -5,6 +5,8 @@
 package com.malhartech.stram;
 
 import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.Collection;
 
 import junit.framework.Assert;
 
@@ -36,8 +38,11 @@ public class DNodeManagerTest {
 
     Assert.assertEquals("number nodes", 3, b.getAllNodes().values().size());
     Assert.assertEquals("number root nodes", 1, b.getRootNodes().size());
-    
-    dnm.addNodes(b.getAllNodes().values());
+
+    Collection<NodeConf> nodes = Arrays.asList(node2, node1);
+    //Collection nodes = b.getAllNodes().values();
+    dnm.addNodes(nodes);
+
     for (NodeConf nodeConf : b.getAllNodes().values()) {
         // required to construct context
         nodeConf.setClassName(TopologyBuilderTest.EchoNode.class.getName());
@@ -45,15 +50,20 @@ public class DNodeManagerTest {
     String container1Id = "container1";
     String container2Id = "container2";
     
-    StreamingContainerContext c1 = dnm.assignContainer(container1Id, new InetSocketAddress(0));
+    // node1 needs to be deployed first, regardless in which order they were given
+    StreamingContainerContext c1 = dnm.assignContainer(container1Id, new InetSocketAddress(container1Id+"Host", 9001));
     Assert.assertEquals("one node assigned to container", 1, c1.getNodes().size());
     Assert.assertEquals("first node assigned to container", "node1", c1.getNodes().get(0).getLogicalId());
     Assert.assertEquals("one stream connection for container1", "n1n2", c1.getStreams().get(0).getId());
+    Assert.assertEquals("stream connects to upstream host", container1Id + "Host", c1.getStreams().get(0).getBufferServerHost());
+    Assert.assertEquals("stream connects to upstream port", 9001, c1.getStreams().get(0).getBufferServerPort());
     
-    StreamingContainerContext c2 = dnm.assignContainer(container2Id, new InetSocketAddress(0));
+    StreamingContainerContext c2 = dnm.assignContainer(container2Id, new InetSocketAddress(container2Id+"Host", 9002));
     Assert.assertEquals("one node assigned to container", 1, c2.getNodes().size());
     Assert.assertEquals("first node assigned to container", "node2", c2.getNodes().get(0).getLogicalId());
     Assert.assertEquals("one stream connection for container2", "n1n2", c2.getStreams().get(0).getId());
+    Assert.assertEquals("stream connects to upstream host", container1Id + "Host", c2.getStreams().get(0).getBufferServerHost());
+    Assert.assertEquals("stream connects to upstream port", 9001, c2.getStreams().get(0).getBufferServerPort());
     
   }
 
