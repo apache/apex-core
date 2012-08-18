337abd7733aeda2f6bab31e455ea9134c39c295a
amol kekre <amol@malhar-inc.com>
2012-08-18 13:53:32 -0700

    Added package-info.java files
    Signed-off-by: amol kekre <amol@malhar-inc.com>

---
+++ engine/src/main/java/com/malhartech/stream/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ * 
+ */
+
+package com.malhartech.stream;
+
---
+++ engine/src/main/java/com/malhartech/util/package-info.java
@@ -0,0 +1,9 @@
+/*
+ *  Copyright (c) 2012 Malhar, Inc.
+ *  All Rights Reserved.
+ * 
+ * @author Chetan Narsude <chetan@malhar-inc.com>
+ */
+
+package com.malhartech.util;
+
