8c514597ce286a5da21eb12d9c6663235828cc3f
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 12:55:58 -0700

    reverting my earlier checkin to this file. essentially removing setObject method. Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/stream/package-info.java
+++ engine/src/main/java/com/malhartech/stream/package-info.java
@@ -2,6 +2,30 @@
  * Copyright (c) 2012-2012 Malhar, Inc.
  * All rights reserved.
  * 
+ * com.malhartech.stream package contains all code related to various implementations of Stream interface. A stream is a logical unit of a dag that defines the connection between
+ * a node and list of listener nodes. Stream has the following properties in Malhar's streaming platform
+ * - One writer node
+ * - Any number of listener nodes
+ * - Properties defined by the context as per the dag specification
+ * 
+ * The streams included in com.malhartech.stream include
+ * 
+ * AbstractActiveMQInputStream: Provides implementation to read from ActiveMQ. Users need to provide getObject implementation. (See example in InputActiveMQStreamTest)
+ * ActiveMQOutputStream: TBD
+ * AbstractHDFSInputStream: Provides implementation of reading from HDFS. Users need to probide getRecord implementation. (See example of HDFSInputStream in com.malhartech.example.wordcount)
+ * BufferServerInputStream: 
+ * BufferServerOutputStream
+ * ConsoleOutputStream
+ * HDFSOutputStream
+ * InlineStream
+ * KafkaInputStream
+ * KafkaOutputStream
+ * SocketInputStream
+ * SocketOutputStream
+ * 
+ * 
+ * A tuple emitted by the writer node is automatically routed to the consumer nodes by the streaming platform
+ * 
  */
 
 package com.malhartech.stream;
