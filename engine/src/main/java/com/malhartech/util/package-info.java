285e7d84d490589886a75df55ba6da73e89d7d25
amol kekre <amol@malhar-inc.com>
2012-08-20 11:46:13 -0700

    Added comments for classes in util package

--- engine/src/main/java/com/malhartech/util/package-info.java
+++ engine/src/main/java/com/malhartech/util/package-info.java
@@ -2,6 +2,11 @@
  *  Copyright (c) 2012 Malhar, Inc.
  *  All Rights Reserved.
  * 
+ * com.malhartech.util package consists of utilities code that Malhar provides and uses internally. The current utilities include
+ * CircularBuffer: Takes a class T and provides a circular buffer. get() on the buffer consumes the object from tail, and add() adds to the head
+ * StablePriorityQueue: Implements a priority queue (Queue<E>) and is mainly used to queue tuples. 
+ * StableWrapper: Used to wrap around long int values safely. Used for windowIds
+ * 
  * @author Chetan Narsude <chetan@malhar-inc.com>
  */
 
