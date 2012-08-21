bb4bac600cf55b69c5c3d637083b2d8ce220709a
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 18:07:02 -0700

    Added comments Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/stram/conf/package-info.java
+++ engine/src/main/java/com/malhartech/stram/conf/package-info.java
@@ -3,6 +3,13 @@
  * All rights reserved.
  * 
  */
+
+/*
+ * com.malhartech.stram.conf is a package that deals with parsing the dag specification and construct dag structure. The same code is used both
+ * on client side and in stram. This ensures that the client side compilation is exactly same as in stram. Any design checks, rules enforcement etc.
+ * would thus be uniforming enforced on hadoop client side as well as in stram 
+ * 
+ */
 package com.malhartech.stram.conf;
 
 
