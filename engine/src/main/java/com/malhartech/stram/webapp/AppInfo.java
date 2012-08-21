3873399f0c93541a7f6cf8d5bc2cd65034797794
Chetan Narsude <chetan@malhar-inc.com>
2012-08-21 10:27:32 -0700

    Started to add class level comments Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/stram/webapp/AppInfo.java
+++ engine/src/main/java/com/malhartech/stram/webapp/AppInfo.java
@@ -2,6 +2,7 @@
  * Copyright (c) 2012-2012 Malhar, Inc.
  * All rights reserved.
  */
+
 package com.malhartech.stram.webapp;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -13,6 +14,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.malhartech.stram.StramAppContext;
+
+
+/**
+ * Provides application level data like user, appId, elapsed time, etc.<p>
+ * <br>Current data includes<br>
+ * <b>Application Id</b><br>
+ * <b>Application Name</b><br>
+ * <b>User Name</b><br>
+ * <b>Start Time</b><br>
+ * <b>Elapsed Time</b><br>
+ * 
+ */
+
 
 @XmlRootElement(name = "info")
 @XmlAccessorType(XmlAccessType.FIELD)
--- engine/src/main/java/com/malhartech/stram/webapp/NodeInfo.java
+++ engine/src/main/java/com/malhartech/stram/webapp/NodeInfo.java
@@ -8,6 +8,19 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * Provides node level data like node id, container id, throughput, etc.<p>
+ * <br>Current data includes<br>
+ * <b>Node Id</b><br>
+ * <b>Node Name</b><br>
+ * <b>Container Id</b><br>
+ * <b>Total Tuples processed</b><br>
+ * <b>Total Bytes processed</b><br>
+ * <b>Node Status</b><br>
+ * <b>Last Heartbeat Time</b><br>
+ * 
+ */
+
 @XmlRootElement(name = "node")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class NodeInfo {
