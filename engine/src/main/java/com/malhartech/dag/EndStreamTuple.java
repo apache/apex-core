d9a42096313ef7758409f24a7cbc73bd94614c7d
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 20:19:16 -0700

    added some comments Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/dag/EndStreamTuple.java
+++ engine/src/main/java/com/malhartech/dag/EndStreamTuple.java
@@ -8,7 +8,10 @@ package com.malhartech.dag;
 import com.malhartech.bufferserver.Buffer;
 
 /**
- *
+ * Defines end of streaming tuple. This is needed to shutdown a stream dynamically. Shutting down a dag can also
+ * be done dynamically by shutting down all the input streams (all inputadapters).
+ * 
+ * 
  * @author Chetan Narsude <chetan@malhar-inc.com>
  */
 public class EndStreamTuple extends Tuple
