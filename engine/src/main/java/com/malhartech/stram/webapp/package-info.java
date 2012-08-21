b442c0f8e61871d7099d6da23292ae6e4e91202a
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 20:31:41 -0700

    added some comments Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/stram/webapp/package-info.java
+++ engine/src/main/java/com/malhartech/stram/webapp/package-info.java
@@ -6,6 +6,7 @@
 
 /**
  * <b>com.malhartech.stram.webapp</b> package is the web service layer of Malhar's streaming platform<p>
+ * <br>
  * The webservices layer interacts with the streaming application master (stram). The internals of Hadoop are taken care
  * off and an common set of functionality is provided for all streaming related runtime data.
  * 
