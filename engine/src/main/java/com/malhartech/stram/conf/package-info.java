250a501a227a0fd0336834065624f8a530350249
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 20:13:28 -0700

    added javadoc complaint comment Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/stram/conf/package-info.java
+++ engine/src/main/java/com/malhartech/stram/conf/package-info.java
@@ -4,10 +4,11 @@
  * 
  */
 
-/*
- * com.malhartech.stram.conf is a package that deals with parsing the dag specification and construct dag structure. The same code is used both
+/**
+ * <b>com.malhartech.stram.conf</b> package deals with parsing the dag specification and construct dag structure. The same code is used both
  * on client side and in stram. This ensures that the client side compilation is exactly same as in stram. Any design checks, rules enforcement etc.
- * would thus be uniforming enforced on hadoop client side as well as in stram 
+ * would thus be uniforming enforced on hadoop client side as well as in stram<br>
+ * <br>
  * 
  */
 package com.malhartech.stram.conf;
