09af4c690b939980fe42e8054d872301e25a6674
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 20:01:45 -0700

    some more comments Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/stram/package-info.java
+++ engine/src/main/java/com/malhartech/stram/package-info.java
@@ -3,4 +3,10 @@
  * All rights reserved.
  * 
  */
+
+/** 
+ * <b>com.malhartech.stram</b> package contains all code for <b>streaming application master</b>. The application master is also called <b>STRAM</b><br>
+ * 
+ * 
+ */
 package com.malhartech.stram;
