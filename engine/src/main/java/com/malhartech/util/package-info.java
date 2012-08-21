80666accd0add7b61f9c499f50e8da669230860b
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 19:41:09 -0700

    Added proper javadoc comments Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/util/package-info.java
+++ engine/src/main/java/com/malhartech/util/package-info.java
@@ -8,10 +8,11 @@
 
 /**
  * 
- * com.malhartech.util package consists of utilities code that Malhar provides and uses internally. The current utilities include
- * CircularBuffer: Takes a class T and provides a circular buffer. get() on the buffer consumes the object from tail, and add() adds to the head
- * StablePriorityQueue: Implements a priority queue (Queue<E>) and is mainly used to queue tuples. 
- * StableWrapper: Used to wrap around long int values safely. Used for windowIds
+ * com.malhartech.util package consists of utilities code that Malhar provides and uses internally.<br>
+ * The current utilities include<br>
+ * <b>CircularBuffer</b>: Takes a class T and provides a circular buffer. get() on the buffer consumes the object from tail, and add() adds to the head<br>
+ * <b>StablePriorityQueue</b>: Implements a priority queue (Queue<E>) and is mainly used to queue tuples.<br> 
+ * <b>StableWrapper</b>: Used to wrap around long int values safely. Used for windowIds<br>
  */
 
 package com.malhartech.util;
