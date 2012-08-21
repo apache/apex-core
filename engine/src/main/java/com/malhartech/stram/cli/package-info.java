6ee8b76c0b6e89dd920c427c6210562d41b7b7af
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 20:12:36 -0700

    added javadoc complaint comment Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/stram/cli/package-info.java
+++ engine/src/main/java/com/malhartech/stram/cli/package-info.java
@@ -2,6 +2,15 @@
  * Copyright (c) 2012-2012 Malhar, Inc.
  * All rights reserved.
  */
+
+/**
+ * <b>com.malhartech.stram.cli</b> package deals with code for command line interface for the streaming platform<br>
+ * The code wraps the webservices layer (<b>com.malhartech.stream.webapp</b>) and thus all access to the start of the current
+ * dag is via one single point. All webservice calls for streaming data go through the stram.
+ * 
+ * 
+ * 
+ */
 package com.malhartech.stram.cli;
 
 
