a283f0196b2c42c8a756e632cc6b68e47832a86a
Chetan Narsude <chetan@malhar-inc.com>
2012-08-20 20:14:14 -0700

    added javadoc complaint comment Signed-off-by:Chetan Narsude <chetan@malhar-inc.com>

--- engine/src/main/java/com/malhartech/stram/webapp/package-info.java
+++ engine/src/main/java/com/malhartech/stram/webapp/package-info.java
@@ -4,5 +4,11 @@
  * 
  */
 
+/**
+ * <b>com.malhartech.stram.webapp</b> package deals with web service layer of Malhar's streaming platform. The webservices layer
+ * interacts with the streaming application master (stram).
+ * 
+ */
+
 package com.malhartech.stram.webapp;
 
