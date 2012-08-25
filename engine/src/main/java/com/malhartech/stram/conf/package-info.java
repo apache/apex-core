6253321164cd75988ede761ecb38e4da9217439a
amol <amol@amol-VirtualBox>
2012-08-24 18:27:43 -0700

    Added links

--- engine/src/main/java/com/malhartech/stram/conf/package-info.java
+++ engine/src/main/java/com/malhartech/stram/conf/package-info.java
@@ -10,6 +10,10 @@
  * <br>
  * The same code is used both client side and in stram. This ensures that the client side compilation is exactly same as in stram. Any design checks, rules enforcement etc.
  * would thus be uniforming enforced on hadoop client side as well as in stram<br>
+ * The com.malhartech.stram.webapp package consists of<br>
+ * <b>{@link com.malhartech.stram.webapp.ShipContainerJars}</b>: Annotation to indicate Stram a jar file dependency that needs to be deployed to cluster<br>
+ * <b>{@link com.malhartech.stram.webapp.TopologyBuilder}</b>: Builder for the DAG logical representation of nodes and streams<br>
+ * <br>
  * <br>
  * 
  */
