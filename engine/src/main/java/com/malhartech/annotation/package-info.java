937c825e7e2d88ac3bc6c93ce9df9fda310e1f79
amol <amol@amol-VirtualBox>
2012-09-04 10:38:12 -0700

    package-info file for com.malhartech.annotation package

---
+++ engine/src/main/java/com/malhartech/annotation/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ */
+
+/**
+ * 
+ * <b>com.malhartech.annotation</b> package defines annotations on DAG objects<p>
+ * <br>
+ * Currrently ports are annotations on the Nodes. In future we may have more<br>
+ * <br>
+ * com.malhartech.annotation package contains the following classes<br>
+ * <b>{@link com.malhartech.annotation.NodeAnnotation}</b>: Annotation for DAG node to declare inputs and outputs. Used for validation and wiring at runtime<br>
+ * <b>{@link com.malhartech.annotation.PortAnnotation}</b>: Annotation definition for port types for Nodes<br>
+ * <br>
+ * 
+ */
+
+
+
+package com.malhartech.annotation;
+
