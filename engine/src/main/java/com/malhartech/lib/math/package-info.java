8e5e3b0765d3bd442831426858e0084b263e67bd
amol <amol@amol-VirtualBox>
2012-09-05 12:10:56 -0700

    added javadoc for package lib.math

---
+++ engine/src/main/java/com/malhartech/lib/math/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ * 
+ */
+
+/** 
+ * <b>com.malhartech.lib.math</b> is a library of arithmetic modules for reuse<p>
+ * <br>
+ * <br>The modules are<br>
+ * <b>{@link com.malhartech.lib.ArithmeticQuotient}</b>: For every window computes quotient of sum of values of a key in two stream<br>
+ * <b>{@link com.malhartech.lib.ArithmeticSum}</b>: For every window adds all the values of a key in a stream. For string values users can choose to appends with a delimiter<br>
+ * <br>  
+ * 
+ */
+
+package com.malhartech.stram;
