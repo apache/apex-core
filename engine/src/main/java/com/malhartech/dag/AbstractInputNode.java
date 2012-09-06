6d49c7c115fd25e0e08dd2b208cff8ae411195d0
amol <amol@amol-VirtualBox>
2012-09-06 10:04:39 -0700

    another comment :)

--- engine/src/main/java/com/malhartech/dag/AbstractInputNode.java
+++ engine/src/main/java/com/malhartech/dag/AbstractInputNode.java
@@ -79,6 +79,7 @@ public abstract class AbstractInputNode implements Node
     afterBeginWindows = null;
     // Should make variable "shutdown" part of AbstrastInputNode, users should not have to override teardown()
     // as they may forget to call super.teardown()
+    // Also move "outputconnected" here as that is a very common need
   }
 
   @Override
