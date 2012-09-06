af9bc148e63125b684f2d9a7bdc0b63db8ca8ef2
amol <amol@amol-VirtualBox>
2012-09-06 15:07:17 -0700

    current state of arithmeticQuotient testing

--- engine/src/test/java/com/malhartech/lib/math/TestArithmeticSum.java
+++ engine/src/test/java/com/malhartech/lib/math/TestArithmeticSum.java
@@ -58,47 +58,53 @@ public class TestArithmeticSum {
         TestSink sumSink = new TestSink();
         node.connect(ArithmeticSum.OPORT_SUM, sumSink);
 
-        HashMap<String, Integer> input = new HashMap<String, Integer>();
+        HashMap<String, Double> input = new HashMap<String, Double>();
 
-        input.put("a", 2); input.put("b", 20); input.put("c", 1000);
+        input.put("a", 2.0); input.put("b", 20.0); input.put("c", 1000.0);
         node.process(input); input.clear();
 
-        input.put("a", 1);
+        input.put("a", 1.0);
         node.process(input); input.clear();
 
-        input.put("a", 10); input.put("b", 5);
+        input.put("a", 10.0); input.put("b", 5.0);
         node.process(input); input.clear();
         
-        input.put("d", 55); input.put("b", 12);
+        input.put("d", 55.0); input.put("b", 12.0);
         node.process(input); input.clear();
  
-        input.put("d", 22);
-        node.process(input); input.clear();
- 
-        input.put("d", 46); input.put("e", 2);
-        node.process(input); input.clear();
-  
-        input.put("a", 23); input.put("d", 4);
-        node.process(input); input.clear();
- 
-        input.put("d", 14);
+        input.put("d", 22.0);
         node.process(input); input.clear();
 
+        input.put("d", 14.2);
+        node.process(input); input.clear();
+
+        // Mix integers and doubles
+        HashMap<String, Integer> inputi = new HashMap<String, Integer>();
+
+        inputi.put("d", 46); inputi.put("e", 2);
+        node.process(inputi); inputi.clear();
+  
+        inputi.put("a", 23); inputi.put("d", 4);
+        node.process(inputi); inputi.clear();
+ 
+
+
         node.endWindow(); // 
-        Assert.assertEquals("number emitted tuples", 5, sumSink.collectedTuples.size()); // for keys "a", "b", "c", "d", "e"
+        // payload should be 1 bag of tuples with keys "a", "b", "c", "d", "e"
+        Assert.assertEquals("number emitted tuples", 1, sumSink.collectedTuples.size());
         for (Object o : sumSink.collectedTuples) {
-            HashMap<String, Integer> output = (HashMap<String, Integer>) o;
-            for (Map.Entry<String, Integer> e : output.entrySet()) {
+            HashMap<String, Number> output = (HashMap<String, Number>) o;
+            for (Map.Entry<String, Number> e : output.entrySet()) {
                 if (e.getKey().equals("a")) {
-                    Assert.assertEquals("emitted value for 'a' was ", new Double(36), e.getValue());
+                    Assert.assertEquals("emitted value for 'a' was ", new Double(36), e.getValue().doubleValue());
                 } else if (e.getKey().equals("b")) {
-                    Assert.assertEquals("emitted tuple for 'b' was ", new Double(37), e.getValue());
+                    Assert.assertEquals("emitted tuple for 'b' was ", new Double(37), e.getValue().doubleValue());
                 } else if (e.getKey().equals("c")) {
-                    Assert.assertEquals("emitted tuple for 'c' was ", new Double(1000), e.getValue());
+                    Assert.assertEquals("emitted tuple for 'c' was ", new Double(1000), e.getValue().doubleValue());
                 } else if (e.getKey().equals("d")) {
-                    Assert.assertEquals("emitted tuple for 'd' was ", new Double(141), e.getValue());
+                    Assert.assertEquals("emitted tuple for 'd' was ", new Double(141.2), e.getValue().doubleValue());
                 } else if (e.getKey().equals("e")) {
-                    Assert.assertEquals("emitted tuple for 'e' was ", new Double(2), e.getValue());
+                    Assert.assertEquals("emitted tuple for 'e' was ", new Double(2), e.getValue().doubleValue());
                 }
             }
         }
