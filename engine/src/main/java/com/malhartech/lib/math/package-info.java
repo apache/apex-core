8a0543bdb44a98b3175b6a4792f1ef9a528edd55
amol <amol@amol-VirtualBox>
2012-09-05 18:22:47 -0700

    fixed javadoc errors

--- engine/src/main/java/com/malhartech/lib/math/package-info.java
+++ engine/src/main/java/com/malhartech/lib/math/package-info.java
@@ -8,8 +8,8 @@
  * <b>com.malhartech.lib.math</b> is a library of arithmetic modules for reuse<p>
  * <br>
  * <br>The modules are<br>
- * <b>{@link com.malhartech.lib.ArithmeticQuotient}</b>: For every window computes quotient of sum of values of a key in two stream<br>
- * <b>{@link com.malhartech.lib.ArithmeticSum}</b>: For every window adds all the values of a key in a stream. For string values users can choose to appends with a delimiter<br>
+ * <b>{@link com.malhartech.lib.math.ArithmeticQuotient}</b>: For every window computes quotient of sum of values of a key in two stream<br>
+ * <b>{@link com.malhartech.lib.math.ArithmeticSum}</b>: For every window adds all the values of a key in a stream. For string values users can choose to appends with a delimiter<br>
  * <br>
  *
  */
