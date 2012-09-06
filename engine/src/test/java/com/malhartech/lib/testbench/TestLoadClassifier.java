b1ae717990ee3db3cf1a4c57a1cf0b0383daf62c
amol <amol@amol-VirtualBox>
2012-09-06 11:37:07 -0700

    tests compile now.

---
+++ engine/src/test/java/com/malhartech/lib/testbench/TestLoadClassifier.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ */
+package com.malhartech.lib.testbench;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.malhartech.dag.NodeConfiguration;
+import com.malhartech.dag.Sink;
+
+/**
+ *
+ */
+public class TestLoadClassifier {
+
+  class TestSink implements Sink {
+    List<Object> collectedTuples = new ArrayList<Object>();
+
+    @Override
+    public void process(Object payload) {
+      collectedTuples.add(payload);
+    }
+
+  }
+
+
+  /**
+   * Test configuration and parameter validation of the node
+   */
+  @Test
+  public void testNodeValidation() {
+
+    NodeConfiguration conf = new NodeConfiguration("mynode", new HashMap<String, String>());
+
+    LoadClassifier node = new LoadClassifier();
+
+/* TBD
+    conf.set(LoadClassifier.KEY_MULTIPLY_BY, "junk");
+
+    try {
+      node.checkConfiguration(conf);
+      Assert.fail("validation error  " + ArithmeticQuotient.KEY_MULTIPLY_BY);
+    } catch (IllegalArgumentException e) {
+      Assert.assertTrue("validate " + ArithmeticQuotient.KEY_MULTIPLY_BY, e.getMessage().contains("expectedErrorSubString"));
+    }
+*/
+  }
+
+  /**
+   * Test node logic emits correct results
+   */
+  @Test
+  public void testNodeProcessing() {
+
+    LoadClassifier node = new LoadClassifier();
+
+    TestSink classifySink = new TestSink();
+    node.connect(LoadClassifier.OPORT_OUT_DATA, classifySink);
+
+    HashMap<String, String> input = new HashMap<String, String>();
+    node.process(input);
+    node.endWindow();
+    Assert.assertEquals("number emitted tuples", 1, classifySink.collectedTuples.size());
+    Assert.assertEquals("emitted tuple", "testtest", classifySink.collectedTuples.get(0));
+
+  }
+
+}
