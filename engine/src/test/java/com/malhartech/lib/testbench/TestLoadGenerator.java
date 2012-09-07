24c0791ef1f3cfdb12ff67499efcd827afbbe6c8
amol <amol@amol-VirtualBox>
2012-09-06 17:38:36 -0700

    Added node conf validation tests

--- engine/src/test/java/com/malhartech/lib/testbench/TestLoadGenerator.java
+++ engine/src/test/java/com/malhartech/lib/testbench/TestLoadGenerator.java
@@ -14,12 +14,17 @@ import org.junit.Test;
 
 import com.malhartech.dag.NodeConfiguration;
 import com.malhartech.dag.Sink;
+import com.malhartech.lib.math.ArithmeticQuotient;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  */
 public class TestLoadGenerator {
-
+  private static Logger LOG = LoggerFactory.getLogger(LoadGenerator.class);
+  
   class TestSink implements Sink {
     List<Object> collectedTuples = new ArrayList<Object>();
 
@@ -38,15 +43,71 @@ public class TestLoadGenerator {
   public void testNodeValidation() {
 
     NodeConfiguration conf = new NodeConfiguration("mynode", new HashMap<String, String>());
-
     LoadGenerator node = new LoadGenerator();
 
+    conf.set(LoadGenerator.KEY_KEYS, "");
+    try {
+      node.myValidation(conf);
+      Assert.fail("validation error  " + LoadGenerator.KEY_KEYS);
+    }
+    catch (IllegalArgumentException e) {
+      Assert.assertTrue("validate " + LoadGenerator.KEY_KEYS,
+                        e.getMessage().contains("is empty"));
+    }  
+ 
+    conf.set(LoadGenerator.KEY_KEYS, "a,b,c,d"); // from now on keys would be a,b,c,d
+    conf.set(LoadGenerator.KEY_WEIGHTS, "10.4,40,20,30");
+    try {
+      node.myValidation(conf);
+      Assert.fail("validation error  " + LoadGenerator.KEY_WEIGHTS);
+    }
+    catch (IllegalArgumentException e) {
+      Assert.assertTrue("validate " + LoadGenerator.KEY_WEIGHTS,
+                        e.getMessage().contains("should be an integer"));
+    }
+    
+    conf.set(LoadGenerator.KEY_WEIGHTS, "10,40,20"); // from now on weights would be 10,40,20,30
+    try {
+      node.myValidation(conf);
+      Assert.fail("validation error  " + LoadGenerator.KEY_WEIGHTS);
+    }
+    catch (IllegalArgumentException e) {
+      Assert.assertTrue("validate " + LoadGenerator.KEY_WEIGHTS,
+                        e.getMessage().contains("does not match number of keys"));
+    }    
 
-    conf.set(LoadGenerator.KEY_KEYS, "a,b,c,d");
+    conf.set(LoadGenerator.KEY_WEIGHTS, ""); // from now on weights would be 10,40,20,30
+    conf.set(LoadGenerator.KEY_VALUES, "a,2,3,4");
+    try {
+      node.myValidation(conf);
+      Assert.fail("validation error  " + LoadGenerator.KEY_VALUES);
+    }
+    catch (IllegalArgumentException e) {
+      Assert.assertTrue("validate " + LoadGenerator.KEY_VALUES,
+                        e.getMessage().contains("should be float"));
+    }
+
+    conf.set(LoadGenerator.KEY_WEIGHTS, "10,40,30,20"); // from now on weights would be 10,40,20,30
+    conf.set(LoadGenerator.KEY_VALUES, "1,2,3");
+    try {
+      node.myValidation(conf);
+      Assert.fail("validation error  " + LoadGenerator.KEY_VALUES);
+    }
+    catch (IllegalArgumentException e) {
+      Assert.assertTrue("validate " + LoadGenerator.KEY_VALUES,
+                        e.getMessage().contains("does not match number of keys"));
+    }    
+    
     conf.set(LoadGenerator.KEY_VALUES, "1,2,3,4");
-    conf.set(LoadGenerator.KEY_WEIGHTS, "10,40,20,30");
-    conf.set(LoadGenerator.KEY_TUPLES_PER_MS, "100");
-
+    conf.set(LoadGenerator.KEY_TUPLES_PER_MS, "0");
+    try {
+      node.myValidation(conf);
+      Assert.fail("validation error  " + LoadGenerator.KEY_VALUES);
+    }
+    catch (IllegalArgumentException e) {
+      Assert.assertTrue("validate " + LoadGenerator.KEY_VALUES,
+                        e.getMessage().contains("has to be > 0"));
+    }   
   }
 
   /**
