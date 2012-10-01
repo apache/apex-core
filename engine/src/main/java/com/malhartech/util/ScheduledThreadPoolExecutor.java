977408b2bf080cde3caf90f8a2cf0480bf06b876
Chetan Narsude <chetan@malhar-inc.com>
2012-10-01 16:40:41 -0700

    Move the reusable code to the library.

--- engine/src/main/java/com/malhartech/util/ScheduledThreadPoolExecutor.java
+++ engine/src/main/java/com/malhartech/util/ScheduledThreadPoolExecutor.java
@@ -4,9 +4,7 @@
  */
 package com.malhartech.util;
 
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
