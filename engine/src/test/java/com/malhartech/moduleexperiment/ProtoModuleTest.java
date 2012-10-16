6c609b500e68bf38f3b43ff97258eec8fdcdfb66
thomas <thomas@malhar-inc.com>
2012-10-15 21:12:59 -0700

    add module dist to produce dist package

--- engine/src/test/java/com/malhartech/moduleexperiment/ProtoModuleTest.java
+++ engine/src/test/java/com/malhartech/moduleexperiment/ProtoModuleTest.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,8 +20,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import scala.actors.threadpool.Arrays;
 
 import com.malhartech.dag.DefaultModuleSerDe;
 import com.malhartech.dag.Sink;
