b2ef879f1c37994862afd556ade8c6f15fdb2cd5
thomas <thomas@malhar-inc.com>
2012-10-20 21:13:36 -0700

    example for configuration injection

---
+++ engine/src/main/java/com/malhartech/annotation/Configurable.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ */
+package com.malhartech.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that the element value can be injected from configuration.
+ * The injector will be provided a configuration accessor for the name.
+ */
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Configurable {
+  /**
+   * The configuration key
+   * @return
+   */
+  public String key();
+
+  /**
+   * If true, and the configuration is not found,
+   * skip injection rather than produce an error.
+   */
+  boolean optional() default true;
+}
