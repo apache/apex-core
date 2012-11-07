4cfa29ee94a5e47e66f60e6f72bb58df833848a8
Chetan Narsude <chetan@malhar-inc.com>
2012-11-06 19:09:18 -0800

    changed the documentation.

--- engine/src/main/java/com/malhartech/api/InputOperator.java
+++ engine/src/main/java/com/malhartech/api/InputOperator.java
@@ -4,21 +4,16 @@
  */
 package com.malhartech.api;
 
-import java.util.Iterator;
-
 /**
  *
  * @author Chetan Narsude <chetan@malhar-inc.com>
  */
 public interface InputOperator extends Operator
 {
-
   /**
-   * During normal operation, the input operators are given an opportunity to emit the tuples
-   * under the control of the thread which controls the operator. The tuples which are emitted
-   * during this call are sent out during the window with id equal to the argument.
+   * The input operators are given an opportunity to emit the tuples which will be marked
+   * as belonging to the windowId identified by immediately preceding beginWindow call.
    *
    */
-
   public void emitTuples();
 }
