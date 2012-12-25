fd92e389aaf566be8d479e0e1f6ca527099a014a
thomas <thomas@malhar-inc.com>
2012-12-24 16:59:57 -0800

    docs

--- engine/src/main/java/com/malhartech/annotation/package-info.java
+++ engine/src/main/java/com/malhartech/annotation/package-info.java
@@ -5,17 +5,9 @@
 
 /**
  *
- * <b>com.malhartech.annotation</b> package defines annotations on DAG objects<p>
+ * Defines annotations for DAG objects
  * <br>
- * Currrently ports are annotations on the Nodes. In future we may have more <br>
- * <br>
- * com.malhartech.annotation package contains the following classes<br>
- * <b>{@link com.malhartech.annotation.ShipContainingJars}</b>: Annotation to indicate a jar file dependency that needs to be deployed to cluster<br>
- * <br>
- *
  */
-
-
 
 package com.malhartech.annotation;
 
---
+++ engine/src/main/java/com/malhartech/api/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2012-2012 Malhar, Inc.
+ * All rights reserved.
+ */
+
+/**
+ *
+ * Defines the platform API for writing operators and applications
+ * <br>
+ */
+
+package com.malhartech.api;
+
