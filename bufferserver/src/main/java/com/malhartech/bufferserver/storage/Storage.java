b41d021439c99c89768d7501b09a79fb7f92415b
Chetan Narsude <chetan@malhar-inc.com>
2013-01-15 11:11:31 -0800

    documented.

--- bufferserver/src/main/java/com/malhartech/bufferserver/storage/Storage.java
+++ bufferserver/src/main/java/com/malhartech/bufferserver/storage/Storage.java
@@ -22,9 +22,11 @@ public interface Storage
 
   /**
    * Store memory block represented by block in non memory storage.
+   * Streaming Platform calls this method to persist the data on secondary storage so the memory
+   * can be freed.
    *
-   * @param Identifier - application specific Id, this need not be unique.
-   * @param uniqueIdentifier - Unique identifier of the block if we want to replace, -1 if storing for the first time.
+   * @param Identifier - application specific Id, this identifies the data source. Identifier is typically the OperatorId.Portname where OperatorId is numerical Id of the operator who port named Portname is generating the data that gets stored in the
+   * @param uniqueIdentifier - unique identifier of the block that needs to be stored. If this identifier is zero, the instance is free to assign it a unique id. If non zero, the instance should use that id to store the block.
    * @param bytes - memory represented as byte array
    * @param start - the offset of the first byte in the array
    * @param end - the offset of the last byte in the array.
@@ -34,14 +36,15 @@ public interface Storage
 
   /**
    *
-   * @param identifier
-   * @param uniqueIdentifier
+   * @param identifier primary identifier of the block which typically identifies the data source.
+   * @param uniqueIdentifier secondary and unique identifier of the block which needs to be retrived.
    * @return memory block which was stored with the passed parameters as identifying information.
    */
   public byte[] retrieve(String identifier, int uniqueIdentifier);
 
   /**
    * Discard the block stored from the secondary storage.
+   * 
    * @param identifier
    * @param uniqueIdentifier
    */
