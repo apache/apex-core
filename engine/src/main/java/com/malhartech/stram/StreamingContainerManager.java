0bab9e1431ede8137531de0f79da90ea367fdbe7
thomas <thomas@malhar-inc.com>
2013-03-20 14:22:25 -0700

    Fix DefaultStreamCodec to use ContextClassLoader (to find custom classes in local mode)

--- engine/src/main/java/com/malhartech/stram/StreamingContainerManager.java
+++ engine/src/main/java/com/malhartech/stram/StreamingContainerManager.java
@@ -26,7 +26,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Predicate;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.malhartech.api.Context.OperatorContext;
 import com.malhartech.api.DAG;
