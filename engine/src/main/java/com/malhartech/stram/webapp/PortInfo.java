0cd137dd629f46ec4320549d0ea2559866185003
David Yan <davidyan@malhar-inc.com>
2013-04-18 19:36:42 -0700

    added port stats, bytes related stats TBD

---
+++ engine/src/main/java/com/malhartech/stram/webapp/PortInfo.java
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (c) 2012-2013 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.malhartech.stram.webapp;
+
+/**
+ *
+ * @author David Yan <davidyan@malhar-inc.com>
+ */
+public class PortInfo
+{
+  public String name;
+  public long totalTuples;
+  public long tuplesPSMA10;
+  public long totalBytes;   // TBD
+  public long bytesPSMA10;  // TBD
+}
