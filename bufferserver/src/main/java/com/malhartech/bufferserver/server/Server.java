f2ca3a5a672cbe5ccfcb7d9f699f828668118da4
Chetan Narsude <chetan@malhar-inc.com>
2013-05-14 18:06:43 -0700

    debugging.

--- bufferserver/src/main/java/com/malhartech/bufferserver/server/Server.java
+++ bufferserver/src/main/java/com/malhartech/bufferserver/server/Server.java
@@ -565,6 +565,7 @@ public class Server implements ServerListener
     @Override
     public void read(int len)
     {
+      logger.debug("read {} bytes", len);
       writeOffset += len;
       do {
         if (size <= 0) {
