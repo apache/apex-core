c44e09e626cf40a1d286d96a30b030bc112f24ca
David Yan <davidyan@malhar-inc.com>
2013-05-23 19:29:16 -0700

    added skeleton request classes for logical plan modification

---
+++ engine/src/main/java/com/malhartech/stram/plan/logical/RemoveOperatorRequest.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2012-2013 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.malhartech.stram.plan.logical;
+
+/**
+ *
+ * @author David Yan <davidyan@malhar-inc.com>
+ */
+public class RemoveOperatorRequest extends LogicalPlanRequest
+{
+  private String operatorName;
+
+  public String getOperatorName()
+  {
+    return operatorName;
+  }
+
+  public void setOperatorName(String operatorName)
+  {
+    this.operatorName = operatorName;
+  }
+
+  @Override
+  public void execute()
+  {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+  
+}
