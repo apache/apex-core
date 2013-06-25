5e8e1a771524e66662847421ed38495f080a7102
thomas <thomas@malhar-inc.com>
2013-06-24 23:26:31 -0700

    ASF license headers: api

--- api/src/main/java/com/datatorrent/api/Component.java
+++ api/src/main/java/com/datatorrent/api/Component.java
@@ -1,6 +1,17 @@
 /*
- *  Copyright (c) 2012 Malhar, Inc.
- *  All Rights Reserved.
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
  */
 package com.datatorrent.api;
 
@@ -10,7 +21,6 @@ package com.datatorrent.api;
  * entities in the system.
  *
  * @param <T1> Context used for the current run of the component.
- * @author Chetan Narsude <chetan@malhar-inc.com>
  */
 public interface Component<T1 extends Context>
 {
--- common/src/main/java/com/datatorrent/common/util/package-info.java
+++ common/src/main/java/com/datatorrent/common/util/package-info.java
@@ -1,10 +1,16 @@
-/**
- * Copyright (c) 2012-2012 Malhar, Inc.
- * All rights reserved.
+/*
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
  *
- */
-/**
- * <b>{@link com.datatorrent.common.util}</b> is a library of utility classes that are open source and shared by all other malhar libraries<p>
- * <br>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
  */
 package com.datatorrent.common.util;
