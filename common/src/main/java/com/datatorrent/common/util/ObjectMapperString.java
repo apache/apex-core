36388383cc480a85a356760a59b2a8382928b623
thomas <thomas@malhar-inc.com>
2013-06-24 23:26:31 -0700

    ASF license headers: api

--- common/src/main/java/com/datatorrent/common/util/ObjectMapperString.java
+++ common/src/main/java/com/datatorrent/common/util/ObjectMapperString.java
@@ -1,12 +1,22 @@
 /*
- *  Copyright (c) 2012-2013 Malhar, Inc.
- *  All Rights Reserved.
+ * Copyright (c) 2013 Malhar Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
  */
 package com.datatorrent.api.util;
 
 /**
  *
- * @author David Yan <davidyan@malhar-inc.com>
  * For JSON raw serialization, assumes the passed string to be a valid javascript value representation
  */
 public class ObjectMapperString
