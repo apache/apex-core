21926027f9c95bdd812db3a3f5bcbfa3f6b365ef
Chetan Narsude <chetan@datatorrent.com>
2013-09-12 13:11:59 -0700

    Segregate NodeRequest into its own top level interface.

---
+++ engine/src/main/java/com/datatorrent/stram/api/NodeRequest.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2012-2013 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.datatorrent.stram.api;
+
+import java.io.IOException;
+
+import com.datatorrent.api.Operator;
+
+/**
+ *
+ * @author Chetan Narsude <chetan@datatorrent.com>
+ */
+public interface NodeRequest
+{
+  enum RequestType
+  {
+    START_RECORDING, STOP_RECORDING, SYNC_RECORDING, SET_PROPERTY
+  }
+
+  /**
+   * Command to be executed at subsequent end of window.
+   *
+   * @param operator
+   * @param id
+   * @param windowId
+   * @throws IOException
+   */
+  public void execute(Operator operator, int id, long windowId) throws IOException;
+
+}
