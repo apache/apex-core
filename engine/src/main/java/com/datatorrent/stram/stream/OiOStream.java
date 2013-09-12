023dfac3c777aa4a26d91e1b3c764f16466f4291
Chetan Narsude <chetan@datatorrent.com>
2013-09-12 11:16:57 -0700

    OiO stream to work with OiO.

--- engine/src/main/java/com/datatorrent/stram/stream/OiOStream.java
+++ engine/src/main/java/com/datatorrent/stram/stream/OiOStream.java
@@ -7,13 +7,20 @@ package com.datatorrent.stram.stream;
 import com.datatorrent.api.Sink;
 import com.datatorrent.stram.engine.Stream;
 import com.datatorrent.stram.engine.StreamContext;
+import com.datatorrent.stram.engine.SweepableReservoir;
+import com.datatorrent.stram.tuple.Tuple;
 
 /**
  * A non buffering stream which facilitates the ThreadLocal implementation of an operator.
+ *
  * @author Chetan Narsude <chetan@datatorrent.com>
  */
-public class OiOStream implements Stream
+public class OiOStream implements Stream, SweepableReservoir
 {
+  public static final String CONTROL_SINK = "CONTROL";
+  private Sink<Object> sink;
+  private Sink<Tuple> control;
+  private int count;
 
   @Override
   public boolean isMultiSinkCapable()
@@ -22,43 +29,84 @@ public class OiOStream implements Stream
   }
 
   @Override
+  @SuppressWarnings({"rawtypes", "unchecked"})
   public void setSink(String id, Sink<Object> sink)
   {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    assert(id != null && CONTROL_SINK.equals(id));
+    Sink temp = sink;
+    control = (Sink<Tuple>)temp;
   }
 
   @Override
-  public void setup(StreamContext t1)
+  public void setup(StreamContext context)
   {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void teardown()
   {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void activate(StreamContext cntxt)
   {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void deactivate()
   {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public void put(Object t)
   {
+    if (t instanceof Tuple) {
+      control.put((Tuple)t);
+    }
+    else {
+      count++;
+      sink.put(t);
+    }
+  }
+
+  @Override
+  public int getCount(boolean reset)
+  {
+    try {
+      return count;
+    }
+    finally {
+      if (reset) {
+        count = 0;
+      }
+    }
+  }
+
+  @Override
+  public Sink<Object> setSink(Sink<Object> sink)
+  {
+    try {
+      return this.sink;
+    }
+    finally {
+      this.sink = sink;
+    }
+  }
+
+  @Override
+  public Tuple sweep()
+  {
     throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
-  public int getCount(boolean bln)
+  public int size()
+  {
+    return 1;
+  }
+
+  @Override
+  public Object remove()
   {
     throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
