e9d6b2145e881e3531e375ccba801f0fd5bd914b
Chetan Narsude <chetan@datatorrent.com>
2013-11-18 12:05:53 -0800

    Use the org.junit.Assert. The junit.framework.Assert is deprecated.

--- engine/src/test/java/com/datatorrent/stram/LocalityTest.java
+++ engine/src/test/java/com/datatorrent/stram/LocalityTest.java
@@ -6,12 +6,12 @@ package com.datatorrent.stram;
 
 import java.io.File;
 import java.util.Map;
-import junit.framework.Assert;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import org.apache.hadoop.yarn.api.records.NodeReport;
