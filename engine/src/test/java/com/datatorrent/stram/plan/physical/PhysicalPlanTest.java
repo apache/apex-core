219b90e68d14813397cd355c4d6ffcbee8301404
Siyuan Hua <siyuan@datatorrent.com>
2014-01-07 15:08:05 -0800

    More dynamic partition test

--- engine/src/test/java/com/datatorrent/stram/plan/physical/PhysicalPlanTest.java
+++ engine/src/test/java/com/datatorrent/stram/plan/physical/PhysicalPlanTest.java
@@ -258,7 +258,7 @@ public class PhysicalPlanTest {
     Assert.assertEquals("" + ctx.deploy, expDeploy, ctx.deploy);
     Assert.assertEquals("Count of storage requests", 2, ctx.backupRequests);
   }
-
+  
   /**
    * Test partitioning of an input operator (no input port).
    * Cover aspects that are not part of generic operator test.
@@ -821,12 +821,12 @@ public class PhysicalPlanTest {
 
   }
 
+  
   /**
    * MxN partitioning. When source and sink of a stream are partitioned, a
    * separate unifier is created container local with each downstream partition.
    */
   @Test
-  @SuppressWarnings("AssignmentToForLoopParameter")
   public void testUnifierPartitioning() {
 
     LogicalPlan dag = new LogicalPlan();
@@ -838,13 +838,16 @@ public class PhysicalPlanTest {
     GenericTestOperator o2 = dag.addOperator("o2", GenericTestOperator.class);
     dag.setAttribute(o2, OperatorContext.INITIAL_PARTITION_COUNT, 3);
     OperatorMeta o2Meta = dag.getMeta(o2);
+    o2Meta.getAttributes().put(OperatorContext.PARTITION_TPS_MIN, 0);
+    o2Meta.getAttributes().put(OperatorContext.PARTITION_TPS_MAX, 5);
 
     dag.addStream("o1.outport1", o1.outport, o2.inport1);
 
     int maxContainers = 10;
     dag.setAttribute(LogicalPlan.CONTAINERS_MAX_COUNT, maxContainers);
 
-    PhysicalPlan plan = new PhysicalPlan(dag, new TestPlanContext());
+    TestPlanContext ctx = new TestPlanContext();
+    PhysicalPlan plan = new PhysicalPlan(dag, ctx);
     Assert.assertEquals("number of containers", 5, plan.getContainers().size());
 
     List<PTOperator> inputOperators = new ArrayList<PTOperator>();
@@ -887,8 +890,28 @@ public class PhysicalPlanTest {
       Assert.assertEquals("input partition keys " + p.getInputs(), null, p.getInputs().get(0).partitions);
       Assert.assertTrue("partitioned unifier container local " + p.getInputs().get(0).source, p.getInputs().get(0).source.isDownStreamInline());
     }
-  }
+    
+    // Test Dynamic change 
+    PTOperator o2p1 = plan.getOperators(o2Meta).get(0);
+    Assert.assertEquals("stats handlers " + o2p1, 1, o2p1.statsListeners.size());
+    StatsListener sl = o2p1.statsListeners.get(0);
+    Assert.assertTrue("stats handlers " + o2p1.statsListeners, sl instanceof PhysicalPlan.PartitionLoadWatch);
+    ((PhysicalPlan.PartitionLoadWatch)sl).evalIntervalMillis = -1; // no delay
 
+    o2p1.stats.tuplesProcessedPSMA = 10;
+    plan.onStatusUpdate(o2p1);
+    Assert.assertEquals("load exceeds max", 1, ctx.events.size());
+    ctx.backupRequests = 0;
+    ctx.events.remove(0).run();
+    
+    for (PTContainer ptContainer : plan.getContainers()) {
+		System.out.println(ptContainer);
+	}
+    Assert.assertEquals("number of containers", 6, plan.getContainers().size());
+    
+  }
+  
+  
   @Test
   public void testCascadingUnifier() {
 
