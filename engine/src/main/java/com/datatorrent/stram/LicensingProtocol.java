ba13640e0472737d08fdb5eb7ff1a26d9a0cee5d
Pramod Immaneni <pramod@datatorrent.com>
2014-02-04 05:49:08 -0800

    SPOI-1842 #comment Moved LicenseProtocol to com.datatorrent.stram package so that reference to license package from obfuscation jar will be removed. Also renamed it to LicensingProtocol.

---
+++ engine/src/main/java/com/datatorrent/stram/LicensingProtocol.java
@@ -0,0 +1,13 @@
+package com.datatorrent.stram;
+
+import org.apache.hadoop.ipc.VersionedProtocol;
+
+/**
+ * Created by Pramod Immaneni <pramod@datatorrent.com> on 1/31/14.
+ */
+public interface LicensingProtocol extends VersionedProtocol
+{
+  public static final long versionID = 201401310447L;
+
+  public byte[] processRequest(byte[] request);
+}
