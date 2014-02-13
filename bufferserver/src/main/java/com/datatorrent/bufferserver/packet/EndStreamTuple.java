4767243304af30cd58abe9af9d5d25533ac6ba40
Chetan Narsude <chetan@datatorrent.com>
2014-02-13 14:32:40 -0800

    Do not optimize the EndStreamTuple.

--- bufferserver/src/main/java/com/datatorrent/bufferserver/packet/EndStreamTuple.java
+++ bufferserver/src/main/java/com/datatorrent/bufferserver/packet/EndStreamTuple.java
@@ -12,18 +12,11 @@ package com.datatorrent.bufferserver.packet;
  */
 public class EndStreamTuple extends WindowIdTuple
 {
-  private static final byte[][] serializedTuples = new byte[16000][];
-
-  static {
-    for (int i = serializedTuples.length; i-- > 0;) {
-      serializedTuples[i] = WindowIdTuple.getSerializedTuple(i);
-      serializedTuples[i][0] = MessageType.END_STREAM_VALUE;
-    }
-  }
-
   public static byte[] getSerializedTuple(int windowId)
   {
-    return serializedTuples[windowId % serializedTuples.length];
+    byte[] serializedTuple = WindowIdTuple.getSerializedTuple(windowId);
+    serializedTuple[0] = MessageType.END_STREAM_VALUE;
+    return serializedTuple;
   }
 
   public EndStreamTuple(byte[] array, int offset, int length)
