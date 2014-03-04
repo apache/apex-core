2a32b9759339a100b4fa7977531ec0fe8bb6012c
Chetan Narsude <chetan@datatorrent.com>
2014-03-04 11:59:42 -0800

    Move the code that belonged to library to library so that API would not pull in random dependencies. The code which stram needs is put under x-java so that even if user does not have malhar, they are able to compile core.

--- common/src/main/java/com/datatorrent/common/util/PubSubWebSocketClient.java
+++ common/src/main/java/com/datatorrent/common/util/PubSubWebSocketClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.datatorrent.api.util;
+package com.datatorrent.lib.util;
 
 import java.io.IOException;
 import java.net.URI;
@@ -29,9 +29,11 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.datatorrent.lib.util.PubSubMessage.PubSubMessageType;
+
 import com.datatorrent.api.Component;
 import com.datatorrent.api.Context;
-import com.datatorrent.api.util.PubSubMessage.PubSubMessageType;
+
 import com.datatorrent.common.util.NameableThreadFactory;
 
 /**
