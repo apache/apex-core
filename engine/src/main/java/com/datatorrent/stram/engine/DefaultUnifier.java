5c06f882f7b0db97211b83950c9eb6c36a9bea26
Chetan Narsude <chetan@datatorrent.com>
2014-04-15 13:51:56 -0700

    Added serialVersionUID.

--- engine/src/main/java/com/datatorrent/stram/engine/DefaultUnifier.java
+++ engine/src/main/java/com/datatorrent/stram/engine/DefaultUnifier.java
@@ -1,13 +1,12 @@
 package com.datatorrent.stram.engine;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
 import com.datatorrent.api.BaseOperator;
 import com.datatorrent.api.DefaultOutputPort;
 import com.datatorrent.api.Operator.Unifier;
+import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
 import com.datatorrent.api.annotation.Stateless;
 
 /**
@@ -30,4 +29,5 @@ public class DefaultUnifier extends BaseOperator implements Unifier<Object>
 
   @SuppressWarnings("unused")
   private static final Logger logger = LoggerFactory.getLogger(DefaultUnifier.class);
+  private static final long serialVersionUID = 201404151351L;
 }
