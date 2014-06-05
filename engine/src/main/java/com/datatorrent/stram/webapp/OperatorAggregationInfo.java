ea20ea3ac4215a128ee13d37e100972d77770def
David Yan <david@datatorrent.com>
2014-06-04 18:10:45 -0700

    SPOI-2585 added automatic aggregation of various physical operator stats

---
+++ engine/src/main/java/com/datatorrent/stram/webapp/OperatorAggregationInfo.java
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ */
+
+package com.datatorrent.stram.webapp;
+
+import java.util.*;
+
+/**
+ *
+ * @author David Yan <david@datatorrent.com>
+ */
+public class OperatorAggregationInfo
+{
+  public static enum Type { MIN, MAX, AVG, SUM };
+  public String name;
+  public EnumMap<Type, Long> tuplesProcessedPSMA = new EnumMap<Type, Long>(Type.class);;
+  public EnumMap<Type, Long> tuplesEmittedPSMA = new EnumMap<Type, Long>(Type.class);;
+  public EnumMap<Type, Double> cpuPercentageMA = new EnumMap<Type, Double>(Type.class);;
+  public EnumMap<Type, Long> latencyMA = new EnumMap<Type, Long>(Type.class);;
+  public EnumMap<Type, Long> lastHeartbeat = new EnumMap<Type, Long>(Type.class);;
+  public EnumMap<Type, Long> failureCount = new EnumMap<Type, Long>(Type.class);;
+  public EnumMap<Type, Long> recoveryWindowId = new EnumMap<Type, Long>(Type.class);;
+  public EnumMap<Type, Long> currentWindowId = new EnumMap<Type, Long>(Type.class);;
+  public Map<String, EnumMap<Type, Number>> counters = new HashMap<String, EnumMap<Type, Number>>();
+}
