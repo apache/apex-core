1afcc26d0b408bc7a049adccae5f31d5e3514d35
Andy Perlitch <andyperlitch@gmail.com>
2014-12-29 17:44:56 -0800

    Add test for required properties from properties.xml.
    Also replace pre-made app package jar with expanded files
    that get compressed into a jar programmatically in the test
    setup.

--- engine/src/test/java/com/datatorrent/stram/client/AppPackageTest.java
+++ engine/src/test/java/com/datatorrent/stram/client/AppPackageTest.java
@@ -7,10 +7,22 @@ package com.datatorrent.stram.client;
 import com.datatorrent.lib.util.JacksonObjectMapperProvider;
 import com.datatorrent.stram.codec.LogicalPlanSerializer;
 import com.datatorrent.stram.plan.logical.LogicalPlan;
-import java.io.File;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import org.apache.commons.io.FileUtils;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  *
@@ -18,14 +30,66 @@ import org.junit.Test;
  */
 public class AppPackageTest
 {
+
+  // file basename for the created jar
+  private static String jarPath = "testAppPackage.jar";
+
+  // The jar file to use for the AppPackage constructor
+  private File file;
+
+  private AppPackage ap;
+  private JacksonObjectMapperProvider jomp;
+  private JSONObject json;
+
+  private class TestMeta extends TestWatcher {
+
+    TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Override
+    protected void starting(Description description) {
+      try {
+
+        // Set up jar file to use with constructor
+        testFolder.create();
+        file = new File(testFolder.getRoot(), jarPath);
+        ZipFile zipFile = new ZipFile(file);
+        ZipParameters zipParameters = new ZipParameters();
+        zipParameters.setIncludeRootFolder(false);
+        zipFile.createZipFileFromFolder("src/test/resources/testAppPackage/testAppPackageSrc", zipParameters, false, Long.MAX_VALUE);
+
+        // Set up test instance
+        ap = new AppPackage(file, true);
+        jomp = new JacksonObjectMapperProvider();
+        jomp.addSerializer(LogicalPlan.class, new LogicalPlanSerializer());
+        json = new JSONObject(jomp.getContext(null).writeValueAsString(ap));
+
+      } catch (ZipException e) {
+        throw new RuntimeException(e);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      } catch (JSONException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    protected void finished(Description description) {
+      super.finished(description);
+      try {
+        FileUtils.forceDelete(file);
+        testFolder.delete();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  @Rule
+  public TestMeta testMeta = new TestMeta();
+
   @Test
   public void testAppPackage() throws Exception
   {
-    AppPackage ap = new AppPackage(new File("src/test/resources/testAppPackage/testAppPackage.jar"), true);
-    JacksonObjectMapperProvider jomp = new JacksonObjectMapperProvider();
-    jomp.addSerializer(LogicalPlan.class, new LogicalPlanSerializer());
-    JSONObject json = new JSONObject(jomp.getContext(null).writeValueAsString(ap));
-
     Assert.assertEquals("pi-demo", json.getString("appPackageName"));
     Assert.assertEquals("1.0-SNAPSHOT", json.getString("appPackageVersion"));
     Assert.assertEquals("1.0.0", json.getString("dtEngineVersion"));
@@ -38,5 +102,12 @@ public class AppPackageTest
     JSONObject dag = application.getJSONObject("dag");
     Assert.assertTrue("There is at least one stream", dag.getJSONArray("streams").length() >= 1);
     Assert.assertTrue("There is at least one operator", dag.getJSONArray("operators").length() >= 1);
+  }
+
+  @Test
+  public void testXmlRequiredProperties()
+  {
+    Set<String> requiredProperties = ap.getRequiredProperties();
+    Assert.assertEquals(2, requiredProperties.size());
   }
 }
