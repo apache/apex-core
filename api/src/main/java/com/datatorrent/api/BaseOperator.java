daa717cc64062c6b899e6f58b755567f30fd96e8
Chetan Narsude <chetan@datatorrent.com>
2015-05-21 16:29:44 -0700

    Separated Serializable functionality from BaseOperator into its own class

--- api/src/main/java/com/datatorrent/api/BaseOperator.java
+++ api/src/main/java/com/datatorrent/api/BaseOperator.java
@@ -15,16 +15,9 @@
  */
 package com.datatorrent.api;
 
-import java.io.ObjectStreamException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Modifier;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.datatorrent.api.Context.OperatorContext;
+
+import com.datatorrent.common.util.SerializableObject;
 
 /**
  * Base class for operator implementations that provides empty implementations
@@ -32,7 +25,7 @@ import com.datatorrent.api.Context.OperatorContext;
  *
  * @since 0.3.2
  */
-public class BaseOperator implements Operator
+public class BaseOperator extends SerializableObject implements Operator
 {
   private String name;
 
@@ -100,107 +93,5 @@ public class BaseOperator implements Operator
     return this.getClass().getSimpleName() + "{name=" + name + '}';
   }
 
-  /**
-   * Create an instance of this operator class by invoking the copy
-   * constructor. If the copy constructor does not exist, this method tries
-   * to get a new instance using default constructor and then initializing all
-   * the non final or non transient fields of the new instance with the
-   * corresponding values from the deserialized operator. Users can override
-   * this method to invoke non default constructor. They can override transferState
-   * to just copy over the non final or non transient values from various fields
-   * of the src object.
-   *
-   * @return an instance of the operator.
-   * @throws ObjectStreamException
-   */
-  public Object readResolve() throws ObjectStreamException
-  {
-    try {
-      Constructor<? extends BaseOperator> constructor = this.getClass().getConstructor(this.getClass());
-
-      try {
-        constructor.setAccessible(true);
-      }
-      catch (SecurityException ex) {
-        logger.warn("Accessing copy constructor {} failed.", constructor, ex);
-      }
-
-      try {
-        return constructor.newInstance(this);
-      }
-      catch (InstantiationException ex) {
-        throw new RuntimeException("Instantiation using copy constructor failed!", ex);
-      }
-      catch (IllegalAccessException ex) {
-        throw new RuntimeException("Instantiation using copy constructor failed!", ex);
-      }
-      catch (IllegalArgumentException ex) {
-        throw new RuntimeException("Instantiation using copy constructor failed!", ex);
-      }
-      catch (InvocationTargetException ex) {
-        throw new RuntimeException("Instantiation using copy constructor failed!", ex);
-      }
-    }
-    catch (NoSuchMethodException snme) {
-      logger.debug("No copy constructor detected for class {}, trying default constructor.", this.getClass().getSimpleName());
-      try {
-        BaseOperator newInstance = this.getClass().newInstance();
-        transferStateTo(newInstance);
-        return newInstance;
-      }
-      catch (IllegalAccessException ex) {
-        throw new RuntimeException("Deserialization using default constructor failed!", ex);
-      }
-      catch (InstantiationException ex) {
-        throw new RuntimeException("Deserialization using default constructor failed!", ex);
-      }
-    }
-  }
-
-  /**
-   * Copy over non final and non transient values from src to dest object.
-   *
-   * @param dest The object to which values are copied.
-   */
-  public void transferStateTo(Object dest)
-  {
-    for (Class<?> clazz = getClass(); !clazz.equals(Object.class); clazz = clazz.getSuperclass()) {
-      Field[] fields = clazz.getDeclaredFields();
-      for (Field field: fields) {
-        final int modifiers = field.getModifiers();
-        if (!(Modifier.isFinal(modifiers) && Modifier.isTransient(modifiers) || Modifier.isStatic(modifiers))) {
-          try {
-            field.setAccessible(true);
-          }
-          catch (SecurityException ex) {
-            logger.warn("Cannot set field {} accessible.", field, ex);
-          }
-
-          try {
-            field.set(dest, field.get(this));
-          }
-          catch (IllegalArgumentException ex) {
-            throw new RuntimeException("Getter/Setter argument failed using reflection on " + field, ex);
-          }
-          catch (IllegalAccessException ex) {
-            throw new RuntimeException("Getter/Setter access failed using reflection on " + field, ex);
-          }
-
-          if (!field.getType().isPrimitive()) {
-            try {
-              field.set(this, null);
-            }
-            catch (IllegalArgumentException ex) {
-              logger.warn("Failed to set field {} to null; generally it's harmless, but with reference counted data structure this may be an issue.", field, ex);
-            }
-            catch (IllegalAccessException ex) {
-              logger.warn("Failed to set field {} to null; generally it's harmless, but with reference counted data structure this may be an issue.", field, ex);
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private static final Logger logger = LoggerFactory.getLogger(BaseOperator.class);
+  private static final long serialVersionUID = 201505211624L;
 }
---
+++ common/src/main/java/com/datatorrent/common/util/SerializableObject.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datatorrent.common.util;
+
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SerializableObject implements Serializable
+{
+  /**
+   * Create an instance of this operator class by invoking the copy
+   * constructor. If the copy constructor does not exist, this method tries
+   * to get a new instance using default constructor and then initializing all
+   * the non final or non transient fields of the new instance with the
+   * corresponding values from the deserialized operator. Users can override
+   * this method to invoke non default constructor. They can override transferState
+   * to just copy over the non final or non transient values from various fields
+   * of the src object.
+   *
+   * @return an instance of the operator.
+   * @throws ObjectStreamException
+   */
+  public Object readResolve() throws ObjectStreamException
+  {
+    try {
+      Constructor<? extends SerializableObject> constructor = this.getClass().getConstructor(this.getClass());
+      try {
+        constructor.setAccessible(true);
+      }
+      catch (SecurityException ex) {
+        logger.warn("Accessing copy constructor {} failed.", constructor, ex);
+      }
+      try {
+        return constructor.newInstance(this);
+      }
+      catch (InstantiationException ex) {
+        throw new RuntimeException("Instantiation using copy constructor failed!", ex);
+      }
+      catch (IllegalAccessException ex) {
+        throw new RuntimeException("Instantiation using copy constructor failed!", ex);
+      }
+      catch (IllegalArgumentException ex) {
+        throw new RuntimeException("Instantiation using copy constructor failed!", ex);
+      }
+      catch (InvocationTargetException ex) {
+        throw new RuntimeException("Instantiation using copy constructor failed!", ex);
+      }
+    }
+    catch (NoSuchMethodException snme) {
+      logger.debug("No copy constructor detected for class {}, trying default constructor.", this.getClass().getSimpleName());
+      try {
+        SerializableObject newInstance = this.getClass().newInstance();
+        transferStateTo(newInstance);
+        return newInstance;
+      }
+      catch (IllegalAccessException ex) {
+        throw new RuntimeException("Deserialization using default constructor failed!", ex);
+      }
+      catch (InstantiationException ex) {
+        throw new RuntimeException("Deserialization using default constructor failed!", ex);
+      }
+    }
+  }
+
+  /**
+   * Copy over non final and non transient values from src to dest object.
+   *
+   * @param dest The object to which values are copied.
+   */
+  public void transferStateTo(Object dest)
+  {
+    for (Class<?> clazz = getClass(); !clazz.equals(Object.class); clazz = clazz.getSuperclass()) {
+      Field[] fields = clazz.getDeclaredFields();
+      for (Field field: fields) {
+        final int modifiers = field.getModifiers();
+        if (!(Modifier.isFinal(modifiers) && Modifier.isTransient(modifiers) || Modifier.isStatic(modifiers))) {
+          try {
+            field.setAccessible(true);
+          }
+          catch (SecurityException ex) {
+            logger.warn("Cannot set field {} accessible.", field, ex);
+          }
+          try {
+            field.set(dest, field.get(this));
+          }
+          catch (IllegalArgumentException ex) {
+            throw new RuntimeException("Getter/Setter argument failed using reflection on " + field, ex);
+          }
+          catch (IllegalAccessException ex) {
+            throw new RuntimeException("Getter/Setter access failed using reflection on " + field, ex);
+          }
+          if (!field.getType().isPrimitive()) {
+            try {
+              field.set(this, null);
+            }
+            catch (IllegalArgumentException ex) {
+              logger.warn("Failed to set field {} to null; generally it's harmless, but with reference counted data structure this may be an issue.", field, ex);
+            }
+            catch (IllegalAccessException ex) {
+              logger.warn("Failed to set field {} to null; generally it's harmless, but with reference counted data structure this may be an issue.", field, ex);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private static final long serialVersionUID = 201505211622L;
+  private static final Logger logger = LoggerFactory.getLogger(SerializableObject.class);
+}
