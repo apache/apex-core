e1f831ca079353bf5d119c8f35b1409e6c8a8faf
Chetan Narsude <chetan@datatorrent.com>
2015-06-19 17:27:58 -0700

    SerializedData shares a whole log of functionality with Slice, so make it extend Slice

--- common/src/main/java/com/datatorrent/common/util/SerializedData.java
+++ common/src/main/java/com/datatorrent/common/util/SerializedData.java
@@ -4,6 +4,8 @@
  */
 package com.datatorrent.common.util;
 
+import com.datatorrent.netlet.util.Slice;
+
 /**
  * Wrapper for a {@code byte[]}, which provides read-only access and can "reveal" a partial slice of the underlying array.<p>
  *
@@ -12,57 +14,17 @@ package com.datatorrent.common.util;
  *
  * @since 0.3.2
  */
-public final class SerializedData
+public final class SerializedData extends Slice
 {
-  /**
-   * the byte buffer where various messages including this one are stored.
-   */
-  public byte[] bytes;
-  /**
-   * the offset at which the current messages's length followed by the actual message is stored.
-   */
-  public int offset;
   /**
    * the offset at which the actual data begins. Between offset and dataOffset, the length of the data is stored.
    */
   public int dataOffset;
-  /**
-   * size is the total size of the slice of the byte array which stores the length and the message.
-   */
-  public int size;
 
   public SerializedData(byte[] array, int offset, int size)
   {
-    bytes = array;
-    this.offset = offset;
-    this.size = size;
+    super(array, offset, size);
   }
 
-  public boolean isEquivalent(SerializedData sd)
-  {
-    if (sd != null) {
-      if (sd.size == size) {
-        for (int i = size; i-- > 0;) {
-          if (sd.bytes[sd.offset + i] != bytes[offset + i]) {
-            return false;
-          }
-        }
-
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  /**
-   *
-   * @return String
-   */
-  @Override
-  public String toString()
-  {
-    return "bytes = " + bytes + " offset = " + offset + " size = " + size;
-  }
-
+  private static final long serialVersionUID = 201596191703L;
 }
