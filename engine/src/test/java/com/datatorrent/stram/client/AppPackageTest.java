acc783596a22c92bd33134db5384760417510f10
DataTorrent CI <jenkins@datatorrent.com>
2015-07-10 17:50:24 +0530

    Preparing to release v3.0.0-RC1

--- engine/src/test/java/com/datatorrent/stram/client/AppPackageTest.java
+++ engine/src/test/java/com/datatorrent/stram/client/AppPackageTest.java
@@ -72,7 +72,7 @@ public class AppPackageTest
   {
     Assert.assertEquals("mydtapp", json.getString("appPackageName"));
     Assert.assertEquals("1.0-SNAPSHOT", json.getString("appPackageVersion"));
-    Assert.assertEquals("2.2.0-SNAPSHOT", json.getString("dtEngineVersion"));
+    Assert.assertEquals("3.0.0-SNAPSHOT", json.getString("dtEngineVersion"));
     Assert.assertEquals("lib/*.jar", json.getJSONArray("classPath").getString(0));
 
     JSONObject application = json.getJSONArray("applications").getJSONObject(0);
