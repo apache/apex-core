5f37bbd3b86d4e563b934ea3f418a965598917d0
David Yan <david@datatorrent.com>
2015-07-17 13:22:53 -0700

    SPOI-5541 removed duplicate code and added unit test for json stream codec

---
+++ common/src/test/java/com/datatorrent/common/codec/JsonStreamCodecTest.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2015 DataTorrent, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.datatorrent.common.codec;
+
+import com.datatorrent.api.StringCodec;
+import java.util.HashMap;
+import java.util.Map;
+import org.codehaus.jettison.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JsonStreamCodecTest
+{
+  static class PojoClass
+  {
+    private InnerProperty x = new InnerProperty();
+    private String s = "xyz";
+
+    public InnerProperty getX()
+    {
+      return x;
+    }
+
+    public String getS()
+    {
+      return s;
+    }
+  }
+
+  static class InnerProperty
+  {
+
+    public int getFoo()
+    {
+      throw new UnsupportedOperationException("test exception");
+    }
+
+    public String getBar()
+    {
+      return "hello world";
+    }
+  }
+
+  static class PojoStringCodec implements StringCodec<InnerProperty>
+  {
+
+    @Override
+    public InnerProperty fromString(String string)
+    {
+      return new InnerProperty();
+    }
+
+    @Override
+    public String toString(InnerProperty pojo)
+    {
+      return "bar:" + pojo.getBar();
+    }
+
+  }
+
+  @Test
+  public void testJsonStreamCodec() throws Exception
+  {
+    Map<Class<?>, Class<? extends StringCodec<?>>> codecs = new HashMap<>();
+    codecs.put(InnerProperty.class, PojoStringCodec.class);
+    JsonStreamCodec jsc = new JsonStreamCodec(codecs);
+    PojoClass obj = new PojoClass();
+    String s = jsc.toByteArray(obj).stringValue();
+    JSONObject json = new JSONObject(s);
+    Assert.assertEquals("xyz", json.getString("s"));
+    Assert.assertEquals("bar:hello world", json.getString("x"));
+  }
+}
