aceaeebe2eda27337bc2dc38f71c11156f983bdb
Timothy Farkas <tim@datatorrent.com>
2015-08-03 10:29:22 -0700

    APEX-26 Generalized PubSubWebsocketServlet for use in Apex and dependent projects.

--- engine/src/main/java/com/datatorrent/stram/util/PubSubWebSocketServlet.java
+++ engine/src/main/java/com/datatorrent/stram/util/PubSubWebSocketServlet.java
@@ -2,7 +2,7 @@
  *  Copyright (c) 2012-2013 DataTorrent, Inc.
  *  All Rights Reserved.
  */
-package com.datatorrent.gateway;
+package com.datatorrent.stram.util;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -11,16 +11,11 @@ import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocket.Connection;
 import org.eclipse.jetty.websocket.WebSocketServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,12 +24,6 @@ import com.datatorrent.common.util.PubSubMessage;
 import com.datatorrent.common.util.PubSubMessage.PubSubMessageType;
 import com.datatorrent.common.util.PubSubMessageCodec;
 
-import com.datatorrent.gateway.security.AuthDatabase;
-import com.datatorrent.gateway.security.AuthenticationException;
-import com.datatorrent.gateway.security.DTPrincipal;
-import com.datatorrent.stram.util.JSONSerializationProvider;
-import com.datatorrent.stram.util.LRUCache;
-
 
 /**
  * <p>PubSubWebSocketServlet class.</p>
@@ -42,7 +31,7 @@ import com.datatorrent.stram.util.LRUCache;
  * @author David Yan <david@datatorrent.com>
  * @since 0.3.2
  */
-public class PubSubWebSocketServlet extends WebSocketServlet
+public class PubSubWebSocketServlet<SECURITY_CONTEXT, PRINCIPAL> extends WebSocketServlet
 {
   private static final Logger LOG = LoggerFactory.getLogger(PubSubWebSocketServlet.class);
   private static final long serialVersionUID = 1L;
@@ -52,10 +41,10 @@ public class PubSubWebSocketServlet extends WebSocketServlet
   private PubSubMessageCodec<Object> codec = new PubSubMessageCodec<Object>(mapper);
   private InternalMessageHandler internalMessageHandler = null;
   private static final int latestTopicCount = 100;
-  private final DTGateway gateway;
-  private static final String AUTH_ATTRIBUTE = "com.datatorrent.auth.principal";
+  protected SECURITY_CONTEXT securityContext;
   private SubscribeFilter subscribeFilter;
   private SendFilter sendFilter;
+  private String authAttribute;
   private final LRUCache<String, Long> latestTopics = new LRUCache<String, Long>(latestTopicCount, false)
   {
     private static final long serialVersionUID = 20140131L;
@@ -69,33 +58,33 @@ public class PubSubWebSocketServlet extends WebSocketServlet
 
   };
 
-  public interface SubscribeFilter
+  public interface SubscribeFilter<SECURITY_CONTEXT, PRINCIPAL>
   {
 
     /**
      * Returns whether or not the principal is allowed to subscribe to this topic
      *
-     * @param gateway
+     * @param securityContext
      * @param principal
      * @param topic
      * @return
      */
-    boolean filter(DTGateway gateway, DTPrincipal principal, String topic);
+    boolean filter(SECURITY_CONTEXT securityContext, PRINCIPAL principal, String topic);
   }
 
-  public interface SendFilter
+  public interface SendFilter<SECURITY_CONTEXT, PRINCIPAL>
   {
 
     /**
      * Returns the data it should be sent given the principal
      *
-     * @param gateway
+     * @param securityContext
      * @param principal
      * @param topic
      * @param data
      * @return the data it should send to the websocket
      */
-    Object filter(DTGateway gateway, DTPrincipal principal, String topic, Object data);
+    Object filter(SECURITY_CONTEXT securityContext, PRINCIPAL principal, String topic, Object data);
   }
 
   public void registerSubscribeFilter(SubscribeFilter filter)
@@ -114,9 +103,10 @@ public class PubSubWebSocketServlet extends WebSocketServlet
 
   }
 
-  public PubSubWebSocketServlet(DTGateway gateway)
+  public PubSubWebSocketServlet(SECURITY_CONTEXT securityContext, String authAttribute)
   {
-    this.gateway = gateway;
+    this.securityContext = securityContext;
+    this.authAttribute = authAttribute;
   }
 
   public void setInternalMessageHandler(InternalMessageHandler internalMessageHandler)
@@ -124,72 +114,21 @@ public class PubSubWebSocketServlet extends WebSocketServlet
     this.internalMessageHandler = internalMessageHandler;
   }
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-  {
-    boolean handled = false;
-    DTPrincipal principal = null;
-    AuthDatabase auth = gateway.getAuthDatabase();
-    if (gateway.isDTSessionHandled()) {
-      //if (DTGateway.WEB_AUTH_TYPE_PASSWORD.equals(gateway.getWebAuthType())) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-          for (Cookie cookie : cookies) {
-            if ("session".equals(cookie.getName())) {
-              try {
-                principal = auth.authenticateSession(cookie.getValue());
-                //request.setAttribute(AUTH_ATTRIBUTE, principal);
-              } catch (AuthenticationException ex) {
-              /* commenting this out to allow anonymous publish from stram
-               throw new WebApplicationException(ex, Status.FORBIDDEN);
-               */
-              }
-              //super.service(request, response);
-            }
-          }
-        }
-      /* commenting this out to allow anonymous publish from stram
-       throw new WebApplicationException(Status.UNAUTHORIZED);
-       */
-      //}
-    } else if (gateway.isHadoopAuthFilterHandled()) {
-      final UserHolder userHolder = new UserHolder();
-      gateway.getHadoopAuthFilter().doFilter(request, response, new FilterChain()
-      {
-        @Override
-        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException
-        {
-          userHolder.username = ((HttpServletRequest)servletRequest).getUserPrincipal().getName();
-        }
-      });
-      if (response.getStatus() == HttpServletResponse.SC_OK) {
-        principal = auth.getUser(userHolder.username);
-      } else {
-        handled = true;
-      }
-    }
-    if (!handled) {
-      if (principal != null) {
-        request.setAttribute(AUTH_ATTRIBUTE, principal);
-      }
-      super.service(request, response);
-    }
-  }
-
-  private class UserHolder {
+  public class UserHolder {
     public String username;
   }
 
   @Override
   public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol)
   {
-    DTPrincipal principal = (DTPrincipal)request.getAttribute(AUTH_ATTRIBUTE);
+    @SuppressWarnings("unchecked")
+    PRINCIPAL principal = (PRINCIPAL) request.getAttribute(authAttribute);
     return new PubSubWebSocket(principal);
   }
 
   private synchronized void subscribe(PubSubWebSocket webSocket, String topic)
   {
-    if (subscribeFilter != null && !subscribeFilter.filter(gateway, webSocket.getPrincipal(), topic)) {
+    if (subscribeFilter != null && !subscribeFilter.filter(securityContext, webSocket.getPrincipal(), topic)) {
       LOG.warn("Subscribe filter returns false for topic {}, user {}. Ignoring subscribe request", topic, webSocket.getPrincipal());
       return;
     }
@@ -289,7 +228,7 @@ public class PubSubWebSocketServlet extends WebSocketServlet
         PubSubWebSocket socket = it.next();
         try {
           if (sendFilter != null) {
-            Object filteredData = sendFilter.filter(gateway, socket.getPrincipal(), topic, data);
+            Object filteredData = sendFilter.filter(securityContext, socket.getPrincipal(), topic, data);
             sendData(socket, topic, filteredData);
           }
           else {
@@ -305,19 +244,19 @@ public class PubSubWebSocketServlet extends WebSocketServlet
     }
   }
 
-  private class PubSubWebSocket implements WebSocket.OnTextMessage
+  protected class PubSubWebSocket implements WebSocket.OnTextMessage
   {
     private Connection connection;
     private final BlockingQueue<String> messageQueue = new ArrayBlockingQueue<String>(1024);
     private final Thread messengerThread = new Thread(new Messenger());
-    private final DTPrincipal principal;
+    private final PRINCIPAL principal;
 
-    public PubSubWebSocket(DTPrincipal principal)
+    public PubSubWebSocket(PRINCIPAL principal)
     {
       this.principal = principal;
     }
 
-    public DTPrincipal getPrincipal()
+    public PRINCIPAL getPrincipal()
     {
       return principal;
     }
