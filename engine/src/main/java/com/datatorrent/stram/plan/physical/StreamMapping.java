3c5bc858967b0dfbebc5422aaaba93fbc7e7a3c8
sandeshh <sandesh.hegde@gmail.com>
2016-08-31 11:26:12 -0700

    changes

--- engine/src/main/java/com/datatorrent/stram/plan/physical/StreamMapping.java
+++ engine/src/main/java/com/datatorrent/stram/plan/physical/StreamMapping.java
@@ -293,7 +293,7 @@ public class StreamMapping implements java.io.Serializable
               }
             }
           }
-          if (!separateUnifiers && ((pks == null || pks.mask == 0) || lastSingle)) {
+          if (!separateUnifiers && lastSingle) {
             if (finalUnifier == null) {
               finalUnifier = createUnifier(streamMeta, plan);
             }
@@ -307,6 +307,11 @@ public class StreamMapping implements java.io.Serializable
           } else {
             // MxN partitioning: unifier per downstream partition
             LOG.debug("MxN unifier for {} {} {}", new Object[] {doperEntry.first, doperEntry.second.getPortName(), pks});
+
+            if (pks != null && pks.mask == 0) {
+              pks = null;
+            }
+
             PTOperator unifier = doperEntry.first.upstreamMerge.get(doperEntry.second);
             if (unifier == null) {
               unifier = createUnifier(streamMeta, plan);
